19db51e8b4ba0461575fedbf4ab55811a55f904b
Felipe Silva <felipe.silvad@outlook.com>
2024-08-22 08:37:26 +0900

    menu

--- src/SoC/Header.js
+++ src/SoC/Header.js
@@ -5,26 +5,68 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Image } from 'react-bootstrap';
 function HeaderComponent() {
   const char_img = require('./assets/img/menu_chars.png')
+  const skills_img = require('./assets/img/menu_skills.png')
+  const event_img = require('./assets/img/menu_events.png')
+  const gear_img = require('./assets/img/menu_gears.png')
+  const tarot_img = require('./assets/img/menu_tarots.png')
   return (
-    <Navbar className='navbar-dark' bg="dark" expand="lg" fixed="top">
+    <Navbar className='navbar-dark menu-navbar' bg="dark" expand="lg" fixed="top">
       <Container className='container'>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/chars" ><Image src={char_img} /> Characters</Nav.Link>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="Skills"
-              menuVariant="dark"
-            >
-              <NavDropdown.Item href="/skills" >Skills</NavDropdown.Item>
-              <NavDropdown.Item href="/traits" >Traits</NavDropdown.Item>
-              <NavDropdown.Item href="/effects" >Effects</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="/gears" >Gears</Nav.Link>
-            <Nav.Link href="/tarots" >Tarots</Nav.Link>
-            <Nav.Link href="/events" >Events</Nav.Link>
+          <Nav className="me-auto menu-nav">
+            <Nav.Link className='nav-link' href="/">
+              <div className='header-link header-link-home'>
+                Home
+              </div>
+            </Nav.Link>
+            <Nav.Link className='nav-link ' href="/chars" >
+              <div className='menu-icon-container'>
+                <Image className='menu-icon' src={char_img} />
+              </div>
+              <div className='header-link'>
+                Characters
+              </div>
+            </Nav.Link>
+            <div className='nav-link'>
+              <div className='menu-icon-container'>
+                <Image className='menu-icon' src={skills_img} />
+              </div>
+              <NavDropdown
+                id="nav-dropdown-dark-example"
+                title="Skills"
+                menuVariant="dark"
+                className='header-link'
+              >
+                <NavDropdown.Item href="/skills" >Skills</NavDropdown.Item>
+                <NavDropdown.Item href="/traits" >Traits</NavDropdown.Item>
+                <NavDropdown.Item href="/effects" >Effects</NavDropdown.Item>
+              </NavDropdown>
+            </div>
+            <Nav.Link className='nav-link' href="/gears" >
+              <div className='menu-icon-container'>
+                <Image className='menu-icon' src={gear_img} />
+              </div>
+              <div className='header-link'>
+                Gears
+              </div>
+            </Nav.Link>
+            <Nav.Link className='nav-link' href="/tarots" >
+              <div className='menu-icon-container'>
+                <Image className='menu-icon' src={tarot_img} />
+              </div>
+              <div className='header-link'>
+                Tarots
+              </div>
+            </Nav.Link>
+            <Nav.Link className='nav-link' href="/events" >
+              <div className='menu-icon-container'>
+                <Image className='menu-icon' src={event_img} />
+              </div>
+              <div className='header-link'>
+                Events
+              </div>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
